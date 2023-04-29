79f31ae3b7b9995a77918127bf0c077cc75a2147
AlvenioFarhan <alveniofarhan@gmail.com>
2023-04-29 22:12:46 +0700

    update

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,16 +1,31 @@
 import {Container, Row, Col} from 'react-bootstrap';
 
+import Logo from '../../assets/Image/Logo.png';
+import Button from 'react-bootstrap/Button';
+import style from './style.module.css';
 
 function Footer() {
   return (
     <>
+    <div className={style.footer}>
       <Container>
+        <Row>
+          <Col lg="6" md="12" sm="12">
+          <Button variant="contained">
+            <img style={{ width: 100, height: 50 }} src={Logo} href="Home" class="center"alt="" />
+          </Button>
+          <p className="footer-address">
+                Jl. Tembalang, Kota Semarang 50271
+              </p>
+          </Col>
+        </Row>
       <Row>
             <Col sm="12" className="textname">
               <p>© {new Date().getFullYear()} | Alvenio </p>
             </Col>
           </Row>
       </Container>
+    </div>
     </>
   );
 }
